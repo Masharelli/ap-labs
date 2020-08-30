316a9f006ea31c2f9b3ffad610e71ae5d4e37f9c
Masharelli <hrocha1997@gmail.com>
2020-08-30 16:44:22 -0500

    Solve word-reverser lab
    Signed-off-by: Masharelli <hrocha1997@gmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,29 @@
+//Rocha Avila Hugo Masahrelli
+//A01633090
+#include <string.h>
 #include <stdio.h>
 
-int main(){
-    // Place your magic here
-    return 0;
+void cambiar(char* a, char*b){
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
 }
+
+void alReves(char* almacenamiento, int tamano ){
+       char* a = almacenamiento;
+       char* b = almacenamiento + tamano - 1;
+       for(char i = 0; i < tamano/2; i++){
+           cambiar(a++, b--);
+       }
+}
+
+ int main(){
+    char c = 0;
+    char almacenamiento[64];
+    while((c = getchar()) != EOF ){
+       scanf("%s", almacenamiento);
+       alReves(almacenamiento,strlen(almacenamiento));
+       printf("Al revez la palabra es: %s\n", almacenamiento);
+       }
+     return 0;
+ }
