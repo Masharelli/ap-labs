a13b5efd12eb7a23ce7d3c49ef94f94fb71433a0
Masharelli <hrocha1997@gmail.com>
2020-09-19 16:14:25 -0500

    Solve go-slices-maps
    Signed-off-by: Masharelli <hrocha1997@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	arr := strings.Fields(s)
+	mapa := make(map[string]int)
+	for _, v := range arr {
+		mapa[v] += 1
+	}
+	return mapa
+}
+
 func main() {
+	wc.Test(WordCount)
 }
