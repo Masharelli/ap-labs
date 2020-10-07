076ddf0a1014020416386f915fa3b79e072cc3a0
Masharelli <hrocha1997@gmail.com>
2020-10-06 21:39:19 -0500

    Solve file-dir-monitor
    Signed-off-by: Masharelli <hrocha1997@gmail.com>

--- labs/file-dir-monitor/logger.c
+++ labs/file-dir-monitor/logger.c
@@ -1,23 +1,114 @@
-#include <stdio.h>
 #include "logger.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <syslog.h>
+#include <signal.h>
 
-int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
-    return 0;
+#define RESET		0
+#define BRIGHT 		1
+#define DIM		    2
+#define UNDERLINE 	3
+#define BLINK		4
+#define REVERSE		7
+#define HIDDEN		8
+
+#define BLACK 		0
+#define RED		    1
+#define GREEN		2
+#define YELLOW		3
+#define BLUE		4
+#define MAGENTA		5
+#define CYAN		6
+#define	WHITE		7
+
+
+int sysType = 0;
+
+void coloTexto(int attr, int fg, int bg){
+	char command[13];
+	sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
+	printf("%s", command);
 }
 
-int infof(const char *format, ...) {
-    return 0;
+int Logger(char *logType) {
+    if (strcmp("syslog", logType) == 0){
+        sysType = 1;
+        printf("Initializing Logger on: %s\n", logType);
+        return 0;
+    } else if (strcmp("stdout", logType) == 0 || strcmp("", logType) == 0){
+        sysType = 0;
+        printf("Initializing Logger on: stdout\n");
+        return 0;
+    } else{
+        printf("Invalid log type, using stdout");
+        return 1;
+    }
+    
 }
+
+
 
 int warnf(const char *format, ...) {
+    coloTexto(BRIGHT, YELLOW, BLACK);
+    va_list args;
+    va_start(args, format);
+    if (sysType == 1){
+        vsyslog(LOG_WARNING,format,args);
+        closelog();
+    } else{
+        vprintf(format, args);
+        printf("\n");
+    }
+    va_end(args);
+    coloTexto(RESET, WHITE, BLACK);
+    return 0;
+}
+int infFIle(const char *format, ...) {
+    coloTexto(UNDERLINE, GREEN, BLACK);
+    va_list args;
+    va_start(args, format);
+    if (sysType == 1){
+        vsyslog(LOG_INFO, format, args);
+        closelog();
+    } else{
+        vprintf(format, args);
+        printf("\n");
+    }
+    va_end(args);
+    coloTexto(RESET, WHITE, BLACK);
     return 0;
 }
 
-int errorf(const char *format, ...) {
-    return 0;
-}
+
 int panicf(const char *format, ...) {
+    coloTexto(BLINK, RED, CYAN);
+    va_list args;
+    va_start(args, format);
+    if (sysType == 1){
+        vsyslog(LOG_CRIT,format,args);
+        closelog();
+    } else{
+        vprintf(format, args);
+        printf("\n");
+    }
+    va_end(args);
+    coloTexto(RESET, WHITE, BLACK);
+    raise(SIGABRT);
     return 0;
 }
-
+int errorf(const char *format, ...) {
+    coloTexto(DIM, RED, BLACK);
+    va_list args;
+    va_start(args, format);
+    if (sysType == 1){
+        vsyslog(LOG_ERR,format,args);
+        closelog();
+    } else{
+        vprintf(format, args);
+        printf("\n");
+    }
+    va_end(args);
+    coloTexto(RESET, WHITE, BLACK);
+    return 0;
+}
--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -1,7 +1,85 @@
+#define _XOPEN_SOURCE 500
+#define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
+#include <ftw.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/inotify.h>
+#include <limits.h>
 #include "logger.h"
 
-int main(){
-    // Place your magic here
-    return 0;
+int inotifyFd, wd;
+
+static void displayInotifyEvent(struct inotify_event *i)
+{
+    if (i->mask & IN_CREATE){
+        infFIle("Archivo creado con el nombre de: %s\n",i->name);
+    }        
+    if (i->mask & IN_DELETE){
+        infFIle("Archivo Borrado: %s\n",i->name);
+    }       
+    if (i->mask & IN_MOVED_FROM){
+        infFIle("Archivo renombrado con el nombre de : %s ",i->name);
+    }   
+    if (i->mask & IN_MOVED_TO){
+        infFIle("Movido al archivo: %s\n", i->name);
+    }
+
+}
+
+
+static int info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
+{
+    if(tflag == FTW_D && ftwbuf->level < 2){ 
+         wd = inotify_add_watch(inotifyFd, fpath, IN_CREATE | IN_DELETE | IN_MOVE);
+        if (wd == -1)
+            errorf("inotify_add_watch");
+    }
+    return 0; 
+}
+
+int main(int argc, char** argv){
+    char buf[BUF_LEN] __attribute__ ((aligned(8)));
+    struct inotify_event *ocurrence;
+    ssize_t changesRead;
+    char *p;
+    
+    int flags = 0;
+
+    if (argc < 2){
+        errorf("Numero equivocado de argumentos");
+        exit(EXIT_FAILURE);
+    }
+
+    inotifyFd = inotify_init();              
+    if (inotifyFd == -1){
+        errorf("inotify_init failed");
+    }
+    if (argc > 2 && strchr(argv[2], 'd') != NULL){
+        flags |= FTW_DEPTH;
+    }
+    if (argc > 2 && strchr(argv[2], 'p') != NULL){
+        flags |= FTW_PHYS;
+    }   
+    if (nftw(argv[1], info, 20, flags) == -1) {
+        errorf("nftw");
+        exit(EXIT_FAILURE);
+    }
+    for (;;) { 
+        changesRead = read(inotifyFd, buf, BUF_LEN);
+        if (changesRead == 0){
+            errorf("read() from inotify fd returned 0!");
+        }
+        if (changesRead == -1){
+            errorf("read failed");
+        }
+        for (p = buf; p < buf + changesRead; ) {
+            ocurrence = (struct inotify_event *) p;
+            displayInotifyEvent(ocurrence);
+
+            p += sizeof(struct inotify_event) + ocurrence->len;
+        }
+    }
+    exit(EXIT_SUCCESS);
 }
