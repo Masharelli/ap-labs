ee9a8b2b709ac09dacaac9477671faae6037bb6f
Masharelli <hrocha1997@gmail.com>
2020-10-12 22:52:05 -0500

    Solve progress-notifier
    Signed-off-by: Masharelli <hrocha1997@gmail.com>

--- labs/progress-notifier/base64.c
+++ labs/progress-notifier/base64.c
@@ -1,7 +1,166 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include <inttypes.h>
+#include <signal.h>
 #include "logger.h"
 
-int main(){
-    // Place your magic here
+#define BUF_SIZE   0xFFFFFF
+#define SIGINFO 29
+
+// Action: decode 2, encode 1. bitwise.
+#define ENCODE 1
+#define DECODE 2
+
+float status = 0;
+
+void sig_handler(int sig) { infof("%d%% of file processed\n", (int) status); }
+
+//Code retrieved from: https://stackoverflow.com/questions/342409/how-do-i-base64-encode-decode-in-c
+static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                                'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                                '4', '5', '6', '7', '8', '9', '+', '/'};
+static char *decoding_table = NULL;
+static int mod_table[] = {0, 2, 1};
+
+void build_decoding_table() {
+
+    decoding_table = malloc(256);
+    int i;
+    for (i = 0; i < 64; i++)
+        decoding_table[(unsigned char) encoding_table[i]] = i;
+}
+
+char *base64_encode(const unsigned char *data, size_t input_length, size_t *output_length) {
+    *output_length = 4 * ((input_length + 2) / 3);
+
+    char *encoded_data = malloc(*output_length);
+    if (encoded_data == NULL) return NULL;
+    int i, j;
+    for (i = 0, j = 0; i < input_length;) {
+        status = (float) i / (float) input_length * 100;
+        uint32_t octet_a = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_b = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_c = i < input_length ? (unsigned char)data[i++] : 0;
+
+        uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+
+        encoded_data[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 1 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
+    }
+
+    for (i = 0; i < mod_table[input_length % 3]; i++)
+        encoded_data[*output_length - 1 - i] = '=';
+
+    return encoded_data;
+}
+
+unsigned char *base64_decode(const char *data,
+                             size_t input_length,
+                             size_t *output_length) {
+    if (decoding_table == NULL) build_decoding_table();
+    *output_length = input_length / 4 * 3;
+    if (data[input_length - 1] == '=') (*output_length)--;
+    if (data[input_length - 2] == '=') (*output_length)--;
+
+    unsigned char *decoded_data = malloc(*output_length);
+    if (decoded_data == NULL) return NULL;
+    int i, j;
+    for (i = 0, j = 0; i < input_length;) {
+        status = (float) i / (float) input_length * 100;
+        uint32_t sextet_a = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_b = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_c = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_d = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+
+        uint32_t triple = (sextet_a << 3 * 6)
+        + (sextet_b << 2 * 6)
+        + (sextet_c << 1 * 6)
+        + (sextet_d << 0 * 6);
+
+        if (j < *output_length) decoded_data[j++] = (triple >> 2 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 1 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 0 * 8) & 0xFF;
+    }
+    return decoded_data;
+}
+
+void base64_cleanup() {
+    free(decoding_table);
+}
+//Here starts my code
+
+
+int main(int argc, char *argv[]) {
+
+    signal(SIGINT, sig_handler); 
+    signal(SIGUSR1, sig_handler);
+    // Action: decode 2, encode 1. bitwise.
+    int action=0;
+    char *output_file = calloc(1024, sizeof(char));
+
+     if(argc!=3){
+        errorf("Error in number of parameters. Aborted\n");
+        return -1;
+     } 
+    if(strcmp(argv[1], "--encode")==0){
+        strcpy(output_file,"encoded.txt");
+        action|=ENCODE;
+    }else if(strcmp(argv[1], "--decode")==0){
+        action|=DECODE;
+        strcpy(output_file,"decoded.txt");            
+    }
+
+    long size;
+    char *buffer;
+    long read_size = 0;
+
+    FILE *file;
+    file = fopen(argv[2],"r");
+    if (file == NULL) {
+        errorf("Couldn't open input file. Aborted\n");
+        return -1;
+    }
+    fseek(file, 0L, SEEK_END);
+    size = ftell(file);
+    fseek(file, 0L, SEEK_SET);
+    buffer = malloc(size);
+    read_size = fread(buffer, 1, size, file);
+    fclose(file);
+
+    size_t output_size = 0;
+
+    char *result;
+    // Action: decode 2, encode 1. bitwise.
+    if (action == ENCODE) {
+        infof("Encoding file\n");
+        result = base64_encode(buffer, strlen(buffer), &output_size);
+    } else if (action == DECODE) {
+        infof("Decoding file\n");
+        result=(char*)  base64_decode(buffer, strlen(buffer), &output_size);
+    } else {
+        errorf("Action flag incorrect. Set as flag --encode or --decode. Aborted\n");
+        return -1;
+    }    
+    base64_cleanup();
+
+    FILE *output;
+    output= fopen(output_file,"w");
+    if (output == NULL) {
+        panicf("Error creando archivo '%s'\n", output_file);
+        return -1;
+    }
+    fwrite(result, output_size, 1, output);
+    infof("Creando archivo '%s'\n", output_file);
+    fclose(output);
     return 0;
 }
--- labs/progress-notifier/logger.c
+++ labs/progress-notifier/logger.c
@@ -1,23 +1,120 @@
 #include <stdio.h>
 #include "logger.h"
+#include <string.h>
+#include <stdarg.h>
+#include <syslog.h>
+#include <signal.h>
+
+
+#define RESET 0
+#define BRIGHT 1
+#define DIM 2
+#define ITALICS 3 //These are the codes my virtual machine thrown.
+#define UNDERLINE 4
+#define BLINK 5
+#define REVERSE 7
+#define HIDDEN 8
+
+#define BLACK 0
+#define RED 1
+#define GREEN 2
+#define YELLOW 3
+#define BLUE 4
+#define MAGENTA 5
+#define CYAN 6
+#define WHITE 7
+#define RESET_BG 8
+
+
+void textcolor(int attr, int fg, int bg);
+int infof(const char *format, ...);
+int warnf(const char *format, ...);
+int errorf(const char *format, ...);
+int panicf(const char *format, ...);
+int initLogger(char *logType);
+
+int isSyslog = 0;
+int sysLogStatus=0;
 
 int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
+    if(strcmp(logType, "syslog") == 0){
+        printf("Initializing Logger on: %s\n", logType);
+        fflush(stdout);
+        isSyslog = 1;
+    }else if(   ( ! strcmp(logType, "stdout") )|| 
+                ( ! strcmp(logType, "") )){
+                printf("Initializing Logger on: %s\n", logType);
+                fflush(stdout);
+                isSyslog = 0;
+    }else{
+        printf("Invalid option. The possible options are: \"stdout\" and \"syslog\".\n");
+        fflush(stdout);
+        return -1;
+    }
     return 0;
 }
 
-int infof(const char *format, ...) {
+int printOn(const char *format, va_list args){
+    if(isSyslog){
+        openlog("myAdvancedLogger",LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(sysLogStatus,format,args);
+        closelog();
+    }else{ 
+        vprintf(format, args); 
+        fflush(stdout);
+        }
     return 0;
 }
 
-int warnf(const char *format, ...) {
+
+void textcolor(int attr, int fg, int bg){
+    char command[13];
+    sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg+30, bg+40);
+    printf("%s", command);
+    fflush(stdout);
+}
+
+int infof(const char *format, ...){
+    sysLogStatus = LOG_INFO;
+    va_list valist;
+    va_start(valist, format);
+    textcolor(RESET,  WHITE, RESET_BG);
+    printOn(format, valist);
+    textcolor(RESET,  WHITE, RESET_BG);
+    va_end(valist);
     return 0;
 }
 
-int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
+int warnf(const char *format, ...){
+    sysLogStatus = LOG_WARNING;
+    va_list valist;
+    va_start(valist, format);
+    textcolor(ITALICS, YELLOW, RESET_BG);
+    printOn(format, valist);
+    textcolor(RESET,  WHITE, RESET_BG);
+    va_end(valist);
     return 0;
 }
 
+int errorf(const char *format, ...){
+    sysLogStatus = LOG_ERR;
+    va_list valist;
+    va_start(valist, format);
+    textcolor(BRIGHT, RED, RESET_BG);
+    printOn(format, valist);
+    textcolor(RESET,  WHITE, RESET_BG);
+    va_end(valist);
+    return 0;
+}
+
+int panicf(const char *format, ...){
+    sysLogStatus = LOG_CRIT;
+    va_list valist;
+    va_start(valist, format);
+    textcolor(BLINK, WHITE, RED);
+    printOn(format, valist);
+    textcolor(RESET,  WHITE, RESET_BG);
+    va_end(valist);
+    raise(SIGABRT);
+    return 0;
+}
